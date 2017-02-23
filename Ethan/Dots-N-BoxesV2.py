4b203f5621ced2aa64abbbe2aef173ab2e1999c1
Ethan Barker <ethanbarker0@gmail.com>
2017-02-22 18:59:20 -0800

    Added custom grid sizes

--- Ethan/Dots-N-BoxesV2.py
+++ Ethan/Dots-N-BoxesV2.py
@@ -1,35 +1,45 @@
 # Imports
+import tkinter as tk
 from turtle import *
 from time import *
 from operator import itemgetter
 import random
 
+grid = 6
 title("Dots-N-Boxes")
 screen = getscreen()
 screen.listen()
 hideturtle() # Hide the turtle icon, cuz it's ugly
 tracer(0,0) # Used to remove turtle animation, update() to refresh screen now
-screen.setworldcoordinates(0,0,500,500) # Set screen cordinates to quadrant 1 only
+screen.setworldcoordinates(0,0,(grid * 100 + 100),(grid * 100 + 100)) # Set screen cordinates to quadrant 1 only
 penup()
 
 # There are two main variables we use here:
 # 1. A seperate variable for each line, which allows easier screen drawing and checking for errors
 # 2. A seperate variable for each box some belonging to more than one line, which allows for easier win checking
-verticals = [0,0,0,0,0,0,0,0,0,0,0,0] # Read from bottom left to upper right
-horizontals = [0,0,0,0,0,0,0,0,0,0,0,0]
-boxes = [0,0,0,0,0,0,0,0,0]
+verticals = []
+horizontals = []
+boxes = []
+for i in range(grid):
+    for i in range(grid-1):
+        verticals.append(0)
+for i in range(grid):
+    for i in range(grid-1):
+        horizontals.append(0)
+for i in range(grid-1):
+    for i in (range(grid-1)):
+        boxes.append(0)
 turn = 1
-grid = 4
 
 def main():
     # Update Score
     printScore()
     goto(100,100)
-    for i in range(1,5):
-        for i in range(1,5):
+    for i in range(1,(grid+1)):
+        for i in range(1,(grid + 1)):
             dot(10)
             forward(100)
-        goto((xcor() - 400),(ycor() + 100))
+        goto((xcor() - (grid * 100)),(ycor() + 100))
 
     # Get input
     onscreenclick(clickHandler)
@@ -38,21 +48,21 @@ def main():
 def clickHandler(rawx,rawy):
     x = int(rawx//1) # Round click to a full int
     y = int(rawy//1)
-    for xnum in range(1,5):
-        tempx = (xnum % 4) + 1
-        for ynum in range(1,4):
-            tempy = (ynum % 3) + 1
+    for xnum in range(1,(grid + 1)):
+        tempx = (xnum % grid) + 1
+        for ynum in range(1,(grid)):
+            tempy = (ynum % (grid-1)) + 1
             if ((tempx * 100) + 10) >= x >= ((tempx * 100) - 10) and ((tempy * 100) + 90) >= y >= ((tempy * 100) + 10):
                 onscreenclick(None)
                 updateVariable(tempx, tempy, True)
-    for xnum in range(1,4):
-        tempx = (xnum % 3) + 1
-        for ynum in range(1,5):
-            tempy = (ynum % 4) + 1
+    for xnum in range(1,(grid)):
+        tempx = (xnum % (grid -1)) + 1
+        for ynum in range(1,(grid + 1)):
+            tempy = (ynum % grid) + 1
             if ((tempx * 100) + 90) >= x >= ((tempx * 100) + 10) and ((tempy * 100) + 10) >= y >= ((tempy * 100) - 10):
                 onscreenclick(None)
                 updateVariable(tempx, tempy, False)
-    goto(250,30)
+    goto((((grid + 1) * 100) / 2),20)
     write("Click a spot", align="center", font=("Arial", 50))
 
 
@@ -61,46 +71,46 @@ def updateVariable(x, y, isVertical):
     global verticals
     global horizontals
     if isVertical:
-        verticals[((y - 1) * 4 + x)-1] = 12
+        verticals[((y - 1) * grid + x)-1] = 1
     else:
-        horizontals[((y - 1) * 3 + x)-1] = 1
+        horizontals[((y - 1) * (grid - 1) + x)-1] = 1
 
     # Draw lines
     clear()
-    for xnum in range(1,5): # 4
-        for ynum in range(1,4): # 3
-            if verticals[((ynum - 1) * 4 + xnum) - 1] != 0:
+    for xnum in range(1,(grid+1)): # 4
+        for ynum in range(1,grid): # 3
+            if verticals[((ynum - 1) * grid + xnum) - 1] != 0:
                 goto((100 * xnum), (100 * ynum))
                 pendown()
                 goto((100 * xnum), (100 * (ynum + 1)))
                 penup()
-    for xnum in range(1,4):
-        for ynum in range(1,5):
-            if horizontals[((ynum - 1) * 3 + xnum) - 1] != 0:
+    for xnum in range(1,grid):
+        for ynum in range(1,(grid + 1)):
+            if horizontals[((ynum - 1) * (grid-1) + xnum) - 1] != 0:
                 goto((100 * xnum), (100 * ynum))
                 pendown()
                 goto((100 * (xnum + 1)), (100 * ynum))
                 penup()
 
     # Check for box filled
-    for i in range(0,9):
+    boxTotal = len(boxes)
+    for i in range(0,boxTotal):
         yMulti = 0
-        if i >= 6:
-            yMulti = 2
-        elif i >= 3:
-            yMulti = 1
-        if (0 not in itemgetter(i,(i+3))(horizontals)) and (0 not in itemgetter((i + yMulti),(i+ yMulti + 1))(verticals)):
+        for z in range(grid-1):
+            if (i >= z * (grid-1)):
+                yMulti += 1
+        if (0 not in itemgetter(i,(i + grid - 1))(horizontals)) and (0 not in itemgetter((i + yMulti-1),(i+ yMulti))(verticals)):
             if boxes[i] == 0:
                 boxes[i] = turn
                 goAgain = True
 
 
     # Draw boxes
-    for xnum in range(1,4):
-        for ynum in range(1,4):
-            pos = ((ynum-1) * 3) + xnum - 1
+    for xnum in range(1,grid):
+        for ynum in range(1,grid):
+            pos = ((ynum-1) * (grid-1)) + xnum - 1
             if boxes[pos] != 0:
-                goto((100 * xnum + 50), (100 * ynum + 50))
+                goto((100 * xnum + 50), (100 * ynum + 20))
                 write(boxes[pos], align="center", font=("Arial", 50))
 
     # Detect a winner
@@ -122,14 +132,14 @@ def updateVariable(x, y, isVertical):
     finishUp(goAgain)
 def printScore():
     score = countScore()
-    goto(100, 450)
+    goto(((grid+1) * 20), ((grid * 100) + 20))
     if turn == 1:
         write("Player 1: " + str(score[0]), align="center", font=("Arial", 50, "bold"))
-        goto(400, 450)
+        goto(((grid+1) * 80), ((grid * 100) + 20))
         write("Player 2: " + str(score[1]), align="center", font=("Arial", 50))
     if turn == 2:
         write("Player 1: " + str(score[0]), align="center", font=("Arial", 50))
-        goto(400, 450)
+        goto(((grid+1) * 80), ((grid * 100) + 20))
         write("Player 2: " + str(score[1]), align="center", font=("Arial", 50, "bold"))
 
 
@@ -154,7 +164,7 @@ def endGame(rawx, rawy):
 def countScore():
     player1 = 0
     player2 = 0
-    for i in range(0,9):
+    for i in range(len(boxes)):
         if boxes[i] == 1:
             player1 += 1
         elif boxes[i] == 2:
